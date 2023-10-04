40164616aafe093a645a58d43491f112441103ea
Saadelsayed99 <saadelsayed9999@gmail.com>
2023-10-04 10:36:32 +0300

    add static & media settings , post images

--- posts/models.py
+++ posts/models.py
@@ -21,6 +21,7 @@ class post(models.Model):
     content = models.TextField(max_length=50000)
     publish_date = models.DateTimeField()
     tags = TaggableManager()
+    image = models.ImageField(upload_to='posts')
     
     def __str__(self):
         return self.title
