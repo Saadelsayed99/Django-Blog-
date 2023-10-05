18fc5f9c311b4908ddee330f9d81f63165beea37
Saadelsayed99 <saadelsayed9999@gmail.com>
2023-10-05 17:37:10 +0300

    add post datail

--- posts/views.py
+++ posts/views.py
@@ -6,3 +6,8 @@ from .models import post
 def post_list(request):
     data = post.objects.all()    # orm ---> sql ---> db ---> list[all posts]
     return render(request,'posts.html',{'posts':data})
+
+
+def post_detail(request,post_id):
+    date = post.objects.get(id=post_id)
+    return render(request,'post_detail.html',{'post':date})
